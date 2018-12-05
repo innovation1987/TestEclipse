9549954d2e145c03a561770c2b168be9c53f4890
innovation1987 <zhurui_idea@163.com>
2018-12-05 17:34:46 +0800

    创建项目

---
+++ src/com/zhurui/DomeHellowrold/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.zhurui.DomeHellowrold;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!");
+	}
+
+}
